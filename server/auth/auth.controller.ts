1e81a36d8fcb4ceefba9f5364fd88b8be0ab2ac0
yehia <yahia.yt@gmail.com>
2025-03-21 22:06:48 +0200

    feat: finishing up backend

--- server/auth/auth.controller.ts
+++ server/auth/auth.controller.ts
@@ -10,7 +10,7 @@ export class AuthController {
   async login(@Body() Body: LoginBody) {
     try {
       return this.authService.login(Body);
-    } catch ({ message: error }) {
+    } catch (error) {
       throw new ConflictException({ status: HttpStatus.INTERNAL_SERVER_ERROR, error });
     }
   }
